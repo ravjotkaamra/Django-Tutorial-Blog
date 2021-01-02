7d814aed49649ba3e37da5c283d48751ad1487cb
ravjot <ravjotkaamra@gmail.com>
2021-01-02 19:36:22 +0530

    Blog to add tutorials

---
+++ main/views.py
@@ -0,0 +1,117 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Tutorial, TutorialCategory, TutorialSeries
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+from .forms import NewUserForm
+
+# Create your views here.
+
+
+def single_slug(request, single_slug):
+    categories = [c.category_slug for c in TutorialCategory.objects.all()]
+    if single_slug in categories:
+        matching_series = TutorialSeries.objects.filter(
+            tutorial_category__category_slug=single_slug
+        )
+
+        series_urls = {}
+        for m in matching_series.all():
+            part_1 = Tutorial.objects.filter(
+                tutorial_series__tutorial_series=m.tutorial_series
+            ).earliest("tutorial_published")
+            series_urls[m] = part_1.tutorial_slug
+
+        return render(
+            request=request,
+            template_name="main/category.html",
+            context={"part_ones": series_urls},
+        )
+
+    tutorials = [t.tutorial_slug for t in Tutorial.objects.all()]
+    if single_slug in tutorials:
+        this_tutorial = Tutorial.objects.get(tutorial_slug=single_slug)
+        tutorials_from_series = Tutorial.objects.filter(
+            tutorial_series__tutorial_series=this_tutorial.tutorial_series
+        ).order_by("tutorial_published")
+        this_tutorial_idx = list(tutorials_from_series).index(this_tutorial)
+
+        return render(
+            request=request,
+            template_name="main/tutorial.html",
+            context={
+                "tutorial": this_tutorial,
+                "sidebar": tutorials_from_series,
+                "this_tut_idx": this_tutorial_idx,
+            },
+        )
+
+    return HttpResponse(f"{single_slug} doesn't correspond to anything.")
+
+
+def homepage(request):
+    return render(
+        request=request,
+        template_name="main/categories.html",
+        context={"categories": TutorialCategory.objects.all},
+    )
+
+
+def register(request):
+
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"New Account Created: {username}")
+            login(request, user)
+            messages.info(request, f"You are now logged in as {username}")
+            return redirect("main:homepage")
+        else:
+            for msg in form.error_messages:
+                messages.error(request, f"{msg}: {form.error_messages[msg]}")
+            return render(
+                request=request,
+                template_name="main/register.html",
+                context={"form": form},
+            )
+
+    form = NewUserForm
+    return render(
+        request=request,
+        template_name="main/register.html",
+        context={"form": form},
+    )
+
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, "Logged out successfully!")
+    return redirect("main:homepage")
+
+
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}")
+                return redirect("main:homepage")
+            else:
+                messages.error(request, "Invalid username or password")
+
+        else:
+            messages.error(request, "Invalid username or password")
+
+    form = AuthenticationForm()
+    return render(
+        request=request,
+        template_name="main/login.html",
+        context={"form": form},
+    )
